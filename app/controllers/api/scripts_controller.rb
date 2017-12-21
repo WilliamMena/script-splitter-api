716c65f96b410d4da01dbac6b871fa3fff9fdfac
WilliamMena <williammena1991@gmail.com>
2017-12-21 00:40:10 -0500

    Finish api/scripts controller actions - rough

---
+++ app/controllers/api/scripts_controller.rb
@@ -0,0 +1,53 @@
+class Api::ScriptsController < ApplicationController
+
+  before_action :set_script, only: [:show, :edit, :update, :destroy]
+
+  def index
+    render json: Script.all
+  end
+
+  def create
+    user = User.find(script_params[:user_id])
+    script = user.scripts.new(script_params)
+    if script.save
+      render json: script
+    else
+      render json: { message: script.errors }, status: 400
+    end
+  end
+
+  def update
+    if @script.update(script_params)
+      render json: @script
+    else
+      render json: { message: @script.errors }, status: 400
+    end
+  end
+
+  def show
+    render json: Script.find(params[:id])
+  end
+
+  def destroy
+    if @script.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to remove this script" }, status: 400
+    end
+  end
+
+
+  private
+
+  def set_script
+    @script = Script.find(params[:id])
+  end
+
+  def script_params
+    params.require(:script).permit(:title, :text, :user_id)
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :scripts, except: [:new, :edit]
+    resources :users, only: [:index, :show]
   end
 end
