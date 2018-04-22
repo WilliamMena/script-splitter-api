f63ede03c9e872d4158b53e897c80c44f90e8f58
WilliamMena <williammena1991@gmail.com>
2018-04-21 20:33:35 -0400

    Give api access to Scipts 'captions'

--- app/models/script.rb
+++ app/models/script.rb
@@ -1,3 +1,4 @@
 class Script < ApplicationRecord
   belongs_to :user
+  has_many :captions
 end
--- app/serializers/script_serializer.rb
+++ app/serializers/script_serializer.rb
@@ -1,4 +1,5 @@
 class ScriptSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :title, :text, :characters
 
+  has_many :captions
 end
