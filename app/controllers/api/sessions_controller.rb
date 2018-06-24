81cd921a27871093b5e4ee40f897f2b4d3f741af
WilliamMena <williammena1991@gmail.com>
2018-06-24 19:34:50 -0400

    Add email to users

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,5 @@
-class API::SessionsController < ApplicationController
+require 'auth'
+class Api::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
