82624107a852e415cb9529e83d136d35b4b920e3
WilliamMena <williammena1991@gmail.com>
2019-01-02 03:05:56 -0500

    Add create user ability to controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,5 +8,21 @@ class Api::UsersController < ApplicationController
     render json: User.find(params[:id])
   end
 
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: { message: user.errors }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 
 end
