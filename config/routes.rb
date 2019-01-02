6b6324c3c90d3537c08f0f17b9d5048cb3a2ecef
WilliamMena <williammena1991@gmail.com>
2019-01-02 03:08:18 -0500

    Add User#Create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :scripts, except: [:new, :edit]
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index, :show, :create]
   end
 end
