40b92b8a425446898328ebb437a9a52f3c8d86e2
WilliamMena <williammena1991@gmail.com>
2019-05-24 11:44:43 -0400

    Change Caption in_point out_point to add_column instead of change

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20190103031505) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "characters"
-    t.time "timecode"
     t.index ["user_id"], name: "index_scripts_on_user_id"
   end
 
@@ -43,8 +42,6 @@ ActiveRecord::Schema.define(version: 20190103031505) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "email"
   end
 
   add_foreign_key "captions", "scripts"
